0618214230398574b5e9153c03ca874a97457467
uzmujizzax-hash <uzmujizzax@gmail.com>
2025-11-05 18:10:47 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero({ onLoginClick }: HeroProps) {
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center space-x-2">
               <GraduationCap className="h-8 w-8 text-blue-600" />
-              <span className="text-2xl font-bold text-gray-800">KS Platform</span>
+              <span className="text-2xl font-bold text-gray-800">Mirzo Ulug'bek nomidagi O'zbekiston Milliy universitetining Jizzax filiali</span>
             </div>
             <button
               onClick={onLoginClick}
